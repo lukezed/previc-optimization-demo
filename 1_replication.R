16b7b63fbe70d1b0e316b020df0219fdf7f791a3
Chi Zhang <chi.zhang.edu@gmail.com>
2025-12-06 23:23:05 +0000

    Initial commit: Replication setup

---
+++ 1_replication.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+read_csv("data/previc_data.csv")
+library(usethis)
+use_git_config(
+  user.name = "Chi Zhang",         
+  user.email = "chi.zhang.edu@gmail.com"   
+)
+use_git()
