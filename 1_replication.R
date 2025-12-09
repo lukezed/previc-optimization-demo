67666482be0e292550667e6a29a52c0cd9f780ac
Chi Zhang <chi.zhang.edu@gmail.com>
2025-12-09 13:45:26 +0000

    my item list

--- 1_replication.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-read_csv("data/previc_data.csv")
-library(usethis)
-use_git_config(
-  user.name = "Chi Zhang",         
-  user.email = "chi.zhang.edu@gmail.com"   
-)
-use_git()
